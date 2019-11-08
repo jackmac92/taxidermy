44125ea5c2fa498c30c8ef75089559414b7aa708
Jack McCown <jmccown@cbinsights.com>
2019-11-08 10:35:11 -0500

    lint fix

--- src/components/Network/index.js
+++ src/components/Network/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import vis from 'vis-network';
 import './styles.css';
 import { Store } from '../../Store';
